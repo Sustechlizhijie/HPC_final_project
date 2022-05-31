4e9bd29030a970b40f7366a9def87b1484b0ce4b
Sustechlizhijie <12132404@mail.sustech.edu.cn>
2022-05-31 23:47:04 +0800

    update_code
    fix the bug in the previous code

--- source_code/implicit_Euler/script.sh
+++ source_code/implicit_Euler/script.sh
@@ -13,9 +13,5 @@ mpirun -np 1 ./implicit_heat.out -ksp_type gmres \
   -ksp_gmres_restart 30 -ksp_rtol 1.0e-10 \
   -ksp_atol 1.0e-50 -ksp_max_it 1500 \
   -ksp_gmres_modifiedgramschmidt \
-  -pc_type asm \
-  -sub_ksp_type richardson \
-  -sub_pc_type icc -ksp_monitor_short \
-  -ksp_converged_reason \
-  -ksp_view \
-  -log_view > $LSB_JOBID.log 2>&1
+  -pc_type jacobi  > $LSB_JOBID.log 2>&1
+
